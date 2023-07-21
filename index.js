2657d40526b3de1ca2a8dd759cfc7774185f9445
Prasad Ruwanpathirana <prasad.ruwanpathirana@gmail.com>
2023-07-21 15:47:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-     return res.status(200).json({branchResult: true});
+     return res.status(200).json({branchResult: false});
 })
 app.listen(process.env.PORT || 3000)
